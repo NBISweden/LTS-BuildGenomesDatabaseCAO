87fe33dc2649cd22b2bf69822c46640e25a925c7
Allison Churcher <allison.churcher@scilifelab.se>
2023-02-09 09:41:24 +0100

    Update make_summary_table.sh
    add check for species whose genomes are listed as 'reference' instead of 'representative'

--- make_summary_table.sh
+++ make_summary_table.sh
@@ -1,8 +1,8 @@
-
 #! /bin/bash/
 
 # This bash script makes an assembly summary table
-# for the target species genomes.
+# for the target species genomes. Note, for eukaryotes
+# there is only one 'representative' or 'reference' genome.
 
 # Run as make_summary_table.sh species_list.txt assembly_summary_genbank.txt outfile.txt
 
@@ -11,6 +11,7 @@ INFILE2=$2
 OUTFILE1=$3
 
 
+# Find 'representative' genomes
 >$OUTFILE1
 while read p
 do
@@ -18,3 +19,10 @@ do
     grep -E "$p" $INFILE2 | grep "representative genome" >> $OUTFILE1
 done < $INFILE1
 
+
+# Find species with 'reference genome'
+while read p
+do
+    # Grep line from assembly summary file
+    grep -E "$p" $INFILE2 | grep "reference genome" >> $OUTFILE1
+done < $INFILE1
